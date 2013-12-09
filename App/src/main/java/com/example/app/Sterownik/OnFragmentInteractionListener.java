e54812490938b2e4d30cc4361bcf742980b5d309
nayk <t_endriu@outlook.com>
2013-12-09 13:50:46 +0100

    Start

---
+++ App/src/main/java/com/example/app/Sterownik/OnFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package com.example.app.Sterownik;
+
+import android.net.Uri;
+
+/**
+ * Created by nayk on 09.12.13.
+ */
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    public void onFragmentInteraction(Uri uri);
+}
---
+++ App/src/main/java/com/example/app/Sterownik/Schemat.java
@@ -0,0 +1,60 @@
+package com.example.app.Sterownik;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Schemat extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    // TODO: Rename and change types and number of parameters
+    public static Schemat newInstance() {
+        Schemat fragment = new Schemat();
+        return fragment;
+    }
+    public Schemat() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schemat, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
